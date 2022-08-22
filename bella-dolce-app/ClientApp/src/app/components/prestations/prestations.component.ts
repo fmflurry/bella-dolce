23fcf6512246d78cf86ce04d5b27d935b177a536
Florian MICHEL <fm.florian.michel@gmail.com>
2022-08-22 11:18:40 +0200

    Add keyword search input so we can focus on desired prestation

--- bella-dolce-app/ClientApp/src/app/components/prestations/prestations.component.ts
+++ bella-dolce-app/ClientApp/src/app/components/prestations/prestations.component.ts
@@ -6,7 +6,6 @@ import { PrestationsService } from "@app/services";
 import { PrestationsStore } from "@app/shared/store/prestations.store";
 import { combineLatest, Observable } from "rxjs";
 import { takeWhile, mergeMap, startWith, map, tap } from "rxjs/operators";
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from "@angular/material/chips";
 
 @Component({
@@ -22,7 +21,6 @@ export class PrestationsComponent implements OnInit, OnDestroy {
   selectedPrestations: Record<string, Prestation> = {};
   categories: string[] = [];
   allCategories: string [] = [];
-  separatorKeysCodes: number[] = [ENTER, COMMA];
   isLoading = true;
 
   category = new FormControl('');
@@ -48,7 +46,7 @@ export class PrestationsComponent implements OnInit, OnDestroy {
     });
 
     // Get prestations' categories
-    this.prestationsService.getPrestationCategories().pipe(
+    const categories$ = this.prestationsService.getPrestationCategories().pipe(
       takeWhile(() => this.isAlive),
       tap(allCategories => {
         this.allCategories = allCategories;
@@ -56,15 +54,14 @@ export class PrestationsComponent implements OnInit, OnDestroy {
         this.filteredCategories$ = this.category.valueChanges.pipe(
           startWith(null),
           map((categoryInput: string | null) => this.filter(categoryInput || '')));
-      })
-    ).subscribe();
+      }));
 
     const allPrestations$ = this.prestationsService.getPrestations();
     const selectedPrestations$ = this.prestationsStore.watch();
 
-    combineLatest([allPrestations$, selectedPrestations$]).pipe(
+    combineLatest([categories$, allPrestations$, selectedPrestations$]).pipe(
       takeWhile(() => this.isAlive),
-    ).subscribe(([allPrestations, selectedPrestations]) => {
+    ).subscribe(([categories, allPrestations, selectedPrestations]) => {
         // stop loading
         this.isLoading = false;
 
@@ -86,10 +83,31 @@ export class PrestationsComponent implements OnInit, OnDestroy {
           return acc;
         }, [] as Prestation[]);
 
-        this.filteredPrestations = this.getFilteredPrestations(this.availablePrestations);
+        setTimeout(() => { this.filteredPrestations = this.getFilteredPrestations(this.availablePrestations); });
 
         this.selectedPrestations = {...selectedPrestations};
     });
+
+    this.tag.valueChanges.pipe(
+      takeWhile(() => this.isAlive)
+    ).subscribe(keyWordInput => {
+      if (!keyWordInput) {
+        this.filteredPrestations = this.availablePrestations;
+      }
+
+      const keywordWithoutDiacritics = this.getLowerWithoutDiacritics(keyWordInput);
+      this.filteredPrestations = this.availablePrestations.filter(ap => {
+        const prestationName = this.getLowerWithoutDiacritics(ap.name);
+        const prestationDescription = this.getLowerWithoutDiacritics(ap.description || '');
+        const prestationCategory = this.getLowerWithoutDiacritics(ap.category.name);
+
+        return prestationName.includes(keywordWithoutDiacritics)
+          || prestationDescription.includes(keywordWithoutDiacritics)
+          || prestationCategory.includes(keywordWithoutDiacritics);
+      });
+    })
+
+
   }
 
   ngOnDestroy() {
@@ -147,15 +165,25 @@ export class PrestationsComponent implements OnInit, OnDestroy {
     return a?.name.toLowerCase().localeCompare(b?.name.toLowerCase());
   }
 
-  private filter(category: string) {
-    const filterCategory = category.toLowerCase();
+  private filter(userInput: string) {
+    let filterCategory = this.getLowerWithoutDiacritics(userInput);
     const categories = this.allCategories.filter(c => this.categories.indexOf(c) === -1);
 
     if (!filterCategory) {
       return categories;
     }
 
-    return categories.filter(c => c.toLowerCase().includes(filterCategory));
+    return categories.filter(c => {
+      let categoryWithoutDiacritics = this.getLowerWithoutDiacritics(c);
+      return categoryWithoutDiacritics.toLowerCase().includes(filterCategory)
+    });
+  }
+
+  private getLowerWithoutDiacritics(word: string) {
+    let wordWithoutDiacritics = word.toLowerCase();
+    wordWithoutDiacritics = wordWithoutDiacritics.replace(new RegExp("[àáâãäå]", 'g'), "a");
+    wordWithoutDiacritics = wordWithoutDiacritics.replace(new RegExp("[èéêë]", 'g'), "e");
+    return wordWithoutDiacritics;
   }
 
 }
